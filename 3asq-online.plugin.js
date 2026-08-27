4ea17b658b4ce8635ff3c3af948a077b99a1188c
naif2211 <naif.com070@gmail.com>
2026-08-27 12:29:12 +0300

    Add initial plugin configuration for 3asq.online

---
+++ 3asq-online.plugin.js
@@ -0,0 +1,14 @@
+{
+  "name": "3asq.online Repo",
+  "plugins": [
+    {
+      "id": "3asq-online",
+      "name": "3asq.online",
+      "version": "1.0.0",
+      "lang": "ar",
+      "nsfw": false,
+      "icon": "https://3asq.online/favicon.ico",
+      "entry": "3asq-online.plugin.js"
+    }
+  ]
+}
